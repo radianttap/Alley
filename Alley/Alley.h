426fbbc6ab86d90c05e6a4bb2848393295e94a60
Aleksandar Vacić <av@radianttap.com>
2019-12-08 13:54:29 +0100

    Initial Commit

---
+++ Alley/Alley.h
@@ -0,0 +1,19 @@
+//
+//  Alley.h
+//  Alley
+//
+//  Created by Aleksandar Vacić on 12/8/19.
+//  Copyright © 2019 Radiant Tap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Alley.
+FOUNDATION_EXPORT double AlleyVersionNumber;
+
+//! Project version string for Alley.
+FOUNDATION_EXPORT const unsigned char AlleyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alley/PublicHeader.h>
+
+
