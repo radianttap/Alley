f94e1e869684a7e9bcc605fef5ee6c0666fc977d
Aleksandar Vacić <av@radianttap.com>
2019-12-11 21:16:52 +0100

    Building simple demo app

---
+++ Demo/AlleyDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlleyDemo
+//
+//  Created by Aleksandar Vacić on 12/11/19.
+//  Copyright © 2019 Radiant Tap. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
