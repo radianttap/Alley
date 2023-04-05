95a7b6cf61b3f266ae0814de5bebfedf4719d245
Aleksandar Vacić <av@radianttap.com>
2023-04-05 18:16:25 +0200

    Added more retryable errors, based on HTTP status-code.
    Also added ability to specify retry delay.

--- Alley/NetworkError-Retries.swift
+++ Alley/NetworkError-Retries.swift
@@ -9,23 +9,39 @@
 import Foundation
 
 public extension NetworkError {
-	///	Returns `true` if URLRequest should be retried for the given `NetworkError` instance.
+	///	How much should we wait before retrying the network call, by default.
+	static var defaultRetryDelay: TimeInterval = 0.5
+	
+	///	Returns `true` if `URLRequest` should be retried for the given `NetworkError` instance.
 	///
 	///	At the lowest network levels, it makes sense to retry for cases of (possible) temporary outage. Things like timeouts, can't connect to host, network connection lost.
 	///	In mobile context, this can happen as you move through the building or traffic and may not represent serious or more permanent connection issues.
 	///
 	///	Upper layers of the app architecture may build on this to add more specific cases when the request should be retried.
 	var shouldRetry: Bool {
-		
 		switch self {
 			case .urlError(let urlError):
 				//	if temporary network issues, retry
 				switch urlError.code {
-					case URLError.timedOut,
-						 URLError.cannotFindHost,
-						 URLError.cannotConnectToHost,
-						 URLError.networkConnectionLost,
-						 URLError.dnsLookupFailed:
+					case .timedOut,
+							.cannotFindHost,
+							.cannotConnectToHost,
+							.networkConnectionLost,
+							.dnsLookupFailed,
+							.notConnectedToInternet:
+						return true
+						
+					default:
+						break
+				}
+				
+			case .endpointError(let httpURLResponse, _):
+				switch httpURLResponse.statusCode {
+					case 408,	// Request Timeout
+						444,	// Connection Closed Without Response
+						503,	// Service Unavailable
+						504,	// Gateway Timeout
+						599:	// Network Connect Timeout Error
 						return true
 						
 					default:
@@ -37,5 +53,10 @@ public extension NetworkError {
 		}
 		
 		return false
+	}
+	
+	///	How much should we wait before retrying the network call.
+	var retryInterval: TimeInterval {
+		return Self.defaultRetryDelay
 	}
 }
