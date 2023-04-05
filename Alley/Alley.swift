345a0b7e48eb2a96e3fc2db4c15d2091da400025
Aleksandar Vacić <av@radianttap.com>
2023-04-05 18:17:52 +0200

    Added async/await support. Added delay.
    By default, each retry is delayed by 0.5s.

--- Alley/Alley.swift
+++ Alley/Alley.swift
@@ -26,12 +26,14 @@ extension URLSession {
 	public typealias NetworkResult = Result<Data, NetworkError>
 	public typealias NetworkCallback = (NetworkResult) -> Void
 
-	/// Executes given URLRequest instance, possibly retrying the said number of times. Through `callback` returns either `Data` from the response or `NetworkError` instance.
+	/// Executes given `URLRequest` instance, possibly retrying the said number of times. Through `callback` returns either `Data` from the response or `NetworkError` instance.
+	///
 	/// If any authentication needs to be done, it's handled internally by this methods and its derivatives.
+	/// 
 	/// - Parameters:
-	///   - urlRequest: URLRequest instance to execute.
+	///   - urlRequest: `URLRequest` instance to execute.
 	///   - maxRetries: Number of automatic retries (default is 10).
-	///   - allowEmptyData: Should empty response `Data` be treated as failure (this is default) even if no other errors are returned by URLSession. Default is `false`.
+	///   - allowEmptyData: Should empty response `Data` be treated as failure (this is default) even if no other errors are returned by `URLSession`. Default is `false`.
 	///   - callback: Closure to return the result of the request's execution.
 	public func performNetworkRequest(
 		_ urlRequest: URLRequest,
@@ -125,11 +127,110 @@ private extension URLSession {
 					}
 
 					//	try again
-					self.execute(newRequest)
+					if networkError.retryInterval == 0 {
+						self.execute(newRequest)
+					} else {
+						DispatchQueue(label: "Alley").asyncAfter(deadline: .now() + networkError.retryInterval) {
+							[weak self] in
+							self?.execute(newRequest)
+						}
+					}
 					return
 				}
 		}
 
 		callback(result)
+	}
+}
+
+//	MARK: Async / Await
+
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+extension URLSession {
+	/// Executes given `URLRequest` instance, possibly retrying the said number of times. Returns `Data` from the response or throws some `NetworkError` instance.
+	///
+	/// If any authentication needs to be done, it's handled internally by this methods and its derivatives.
+	///
+	/// - Parameters:
+	///   - urlRequest: `URLRequest` instance to execute.
+	///   - maxRetries: Number of automatic retries (default is 10).
+	///   - allowEmptyData: Should empty response `Data` be treated as failure (this is default) even if no other errors are returned by `URLSession`. Default is `false`.
+	public func performNetworkRequest(_ urlRequest: URLRequest, maxRetries: Int = 10, allowEmptyData: Bool = false) async throws -> Data {
+		let networkRequest = RetriableRequest(
+			urlRequest,
+			1,
+			maxRetries,
+			allowEmptyData
+		)
+		
+		return try await execute(networkRequest)
+	}
+}
+
+@available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+private extension URLSession {
+
+	typealias RetriableRequest = (
+		urlRequest: URLRequest,
+		currentRetries: Int,
+		maxRetries: Int,
+		allowEmptyData: Bool
+	)
+
+	///
+	func execute(_ networkRequest: RetriableRequest) async throws -> Data {
+		let urlRequest = networkRequest.urlRequest
+		
+		do {
+			let (data, urlResponse) = try await data(for: urlRequest)
+			try verify(data, urlResponse, for: networkRequest)
+			return data
+			
+		} catch let err as NetworkError {
+			return try await retry(networkRequest, ifPossibleFor: err)
+
+		} catch let err as URLError {
+			return try await retry(networkRequest, ifPossibleFor: NetworkError.urlError(err))
+
+		} catch let err {
+			return try await retry(networkRequest, ifPossibleFor: NetworkError.generalError(err))
+		}
+	}
+
+	///
+	func verify(_ data: Data, _ urlResponse: URLResponse, for networkRequest: RetriableRequest) throws {
+	
+		guard let httpURLResponse = urlResponse as? HTTPURLResponse else {
+			throw NetworkError.invalidResponseType(urlResponse)
+		}
+		
+		if httpURLResponse.statusCode >= 400 {
+			throw NetworkError.endpointError(httpURLResponse, data)
+		}
+		
+		if data.isEmpty, !networkRequest.allowEmptyData {
+			throw NetworkError.noResponseData(httpURLResponse)
+		}
+	}
+	
+	///
+	func retry(_ networkRequest: RetriableRequest, ifPossibleFor err: NetworkError) async throws -> Data {
+		guard err.shouldRetry else {
+			throw err
+		}
+
+		//	update retries count
+		var newRequest = networkRequest
+		newRequest.currentRetries += 1
+		
+		if newRequest.currentRetries >= newRequest.maxRetries {
+			throw NetworkError.inaccessible
+		}
+		
+		if err.retryInterval > 0 {
+			try await Task.sleep(nanoseconds: UInt64(err.retryInterval * 1_000_000_000))
+		}
+
+		return try await execute(newRequest)
 	}
 }
